8bc2f26fbfd0162fa355a65568a785ad1ce29758
Alp <besiktasalp1903@gmail.com>
2021-01-23 09:57:36 +0300

    frog level music jump sound improved

---
+++ Assets/Scripts/SoundManager/SoundActivate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundActivate : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("frog"))
+        {
+            AudioManager.Instance.PlaySound(Sounds.Level_2_background, transform.position);
+        }
+    }
+}
